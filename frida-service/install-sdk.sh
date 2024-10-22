226e5bf2a59c03c109c8364bdf338df48f11a349
tiketdataannas <annas.adharuqudni@tiket.com>
2024-10-22 19:04:04 +0700

    add sdk tools

--- frida-service/install-sdk.sh
+++ frida-service/install-sdk.sh
@@ -12,5 +12,6 @@ if [ "$INSTALL_ANDROID_SDK" == "1" ]; then
   unzip -d $ANDROID_SDK_ROOT/cmdline-tools/ /tmp/commandlinetools-linux-${CMD_LINE_VERSION}.zip && \
   mv $ANDROID_SDK_ROOT/cmdline-tools/cmdline-tools/ $ANDROID_SDK_ROOT/cmdline-tools/tools/ && \
   rm /tmp/commandlinetools-linux-${CMD_LINE_VERSION}.zip && \
-  yes | sdkmanager --licenses
+  yes | sdkmanager --licenses && \
+  sdkmanager --install "$PACKAGE_PATH" "$ANDROID_PLATFORM_VERSION" platform-tools emulator
 fi
